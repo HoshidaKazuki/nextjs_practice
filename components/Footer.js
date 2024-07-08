a1653ccd1ccd346978ccb1ec0928f9815d7b8c23
HoshidaKazuki <k.hoshida521@gmail.com>
2024-07-08 13:49:37 +0900

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="w-full text-center bg-whitesmoke py-2 md:hidden">
+      <p>&copy; 2024 Hoshida.Inc</p>
+    </div>
+  );
+}
+
+export default Footer;
